86425b7a4b8ba3aeb71e63146c682bd9fe67ca51
ana carter <aecarter@gmail.com>
2012-04-22 10:13:59 -0700

    portfolio page 99% complete. Need to enter proper descriptions.

--- public/javascripts/dialog.js
+++ public/javascripts/dialog.js
@@ -21,12 +21,31 @@ var Dialog = function() {
       this.dialog_lanecrawford_teaser();
       this.dialog_grandsf();
       this.dialog_newlywish();
+      this.dialog_luckyzinc();
+      this.dialog_carlosfalchi();
+      this.dialog_carlosfalchi_teaser();
+      this.dialog_coolercleanse();
+      this.dialog_chilewich_wholesale();
+      this.dialog_lucky_stylespotter();
+      this.dialog_orsella();
+      this.dialog_bond_and_bowery();
+      this.dialog_chilewich();
+      this.dialog_thornton();
+      this.dialog_liz();
+      this.dialog_citibabes();
+      this.dialog_mally_v1();
+      this.dialog_kates();
+      this.dialog_kimara();
       this.dialog_bw_collage1();
       this.dialog_bw_collage2();
       this.dialog_bw_collage3();
       this.dialog_color_collage1();
       this.dialog_color_collage2();
       this.dialog_color_collage3();
+      this.dialog_sycamore();
+      this.dialog_walnut();
+      this.dialog_walnut_shelves();
+      this.dialog_cherry();
     },
     
     dialog_wedding_site: function() {   
@@ -329,6 +348,306 @@ var Dialog = function() {
       });
     },
     
+    dialog_luckyzinc: function() {   
+      $('#luckyzinc').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#luckyzinc_opener').live('click', function() {
+    	  Dialog.open('#luckyzinc');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#luckyzinc');
+        return false;
+      });
+    },
+    
+    dialog_carlosfalchi: function() {   
+      $('#carlosfalchi').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#carlosfalchi_opener').live('click', function() {
+    	  Dialog.open('#carlosfalchi');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#carlosfalchi');
+        return false;
+      });
+    },
+    
+    dialog_carlosfalchi_teaser: function() {   
+      $('#carlosfalchi_teaser').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#carlosfalchi_teaser_opener').live('click', function() {
+    	  Dialog.open('#carlosfalchi_teaser');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#carlosfalchi_teaser');
+        return false;
+      });
+    },
+    
+    dialog_coolercleanse: function() {   
+      $('#coolercleanse').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#coolercleanse_opener').live('click', function() {
+    	  Dialog.open('#coolercleanse');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#coolercleanse');
+        return false;
+      });
+    },
+    
+    dialog_chilewich_wholesale: function() {   
+      $('#chilewich_wholesale').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#chilewich_wholesale_opener').live('click', function() {
+    	  Dialog.open('#chilewich_wholesale');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#chilewich_wholesale');
+        return false;
+      });
+    },
+    
+    dialog_lucky_stylespotter: function() {   
+      $('#lucky_stylespotter').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#lucky_stylespotter_opener').live('click', function() {
+    	  Dialog.open('#lucky_stylespotter');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#lucky_stylespotter');
+        return false;
+      });
+    },
+
+    dialog_orsella: function() {   
+      $('#orsella').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#orsella_opener').live('click', function() {
+    	  Dialog.open('#orsella');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#orsella');
+        return false;
+      });
+    },
+    
+    dialog_bond_and_bowery: function() {   
+      $('#bond_and_bowery').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#bond_and_bowery_opener').live('click', function() {
+    	  Dialog.open('#bond_and_bowery');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#bond_and_bowery');
+        return false;
+      });
+    },
+    
+    dialog_chilewich: function() {   
+      $('#chilewich').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#chilewich_opener').live('click', function() {
+    	  Dialog.open('#chilewich');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#chilewich');
+        return false;
+      });
+    },
+    
+    dialog_thornton: function() {   
+      $('#thornton').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#thornton_opener').live('click', function() {
+    	  Dialog.open('#thornton');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#thornton');
+        return false;
+      });
+    },
+    
+    dialog_liz: function() {   
+      $('#liz').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#liz_opener').live('click', function() {
+    	  Dialog.open('#liz');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#liz');
+        return false;
+      });
+    },
+    
+    dialog_citibabes: function() {   
+      $('#citibabes').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#citibabes_opener').live('click', function() {
+    	  Dialog.open('#citibabes');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#citibabes');
+        return false;
+      });
+    },
+    
+    dialog_mally_v1: function() {   
+      $('#mally_v1').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#mally_v1_opener').live('click', function() {
+    	  Dialog.open('#mally_v1');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#mally_v1');
+        return false;
+      });
+    },
+    
+    dialog_kates: function() {   
+      $('#kates').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#kates_opener').live('click', function() {
+    	  Dialog.open('#kates');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#kates');
+        return false;
+      });
+    },
+    
+    dialog_kimara: function() {   
+      $('#kimara').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 780,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#kimara_opener').live('click', function() {
+    	  Dialog.open('#kimara');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#kimara');
+        return false;
+      });
+    },
+    
     dialog_bw_collage1: function() {   
       $('#bwcollage1').dialog({
         autoOpen: false,
@@ -447,6 +766,90 @@ var Dialog = function() {
         Dialog.close('#color_collage3');
         return false;
       });
+    },
+    
+    dialog_sycamore: function() {   
+      $('#sycamore').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 660,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#sycamore_opener').live('click', function() {
+    	  Dialog.open('#sycamore');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#sycamore');
+        return false;
+      });
+    },
+    
+    dialog_walnut: function() {   
+      $('#walnut').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 660,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#walnut_opener').live('click', function() {
+    	  Dialog.open('#walnut');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#walnut');
+        return false;
+      });
+    },
+    
+    dialog_walnut_shelves: function() {   
+      $('#walnut_shelves').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 660,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#walnut_shelves_opener').live('click', function() {
+    	  Dialog.open('#walnut_shelves');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#walnut_shelves');
+        return false;
+      });
+    },
+    
+    dialog_cherry: function() {   
+      $('#cherry').dialog({
+        autoOpen: false,
+      	modal: true,
+      	resizable: false,
+      	width: 660,
+      	position:  [ 'center', 'center' ]
+      });
+      
+      $('#cherry_opener').live('click', function() {
+    	  Dialog.open('#cherry');
+        return false;
+    	});
+    	
+    	$('#js-close-modal').live('click', function() {
+        Dialog.close('#cherry');
+        return false;
+      });
+    },
+    
+    open: function(dialog_name) {
+      $(dialog_name).dialog('open');
     },
     
     open: function(dialog_name) {
